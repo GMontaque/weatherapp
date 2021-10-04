389424f31cf81ebc5e07f16a579c441e5f1db609
Giulio <62805702+knightrider909@users.noreply.github.com>
2021-10-04 13:23:55 +0100

    setup

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import "./SearchBar.css";
+
+function SearchBar() {
+	return (
+		<div>
+			<input placeholder="search"></input>
+			<button type="button" class="btn btn-primary">
+				Submit
+			</button>
+		</div>
+	);
+}
+
+export default SearchBar;
